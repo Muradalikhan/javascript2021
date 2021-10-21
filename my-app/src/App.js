c5587390cb3f18e0d20baeb473b69226fab3af2d
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2021-10-21 12:03:28 +0500

    componant of card
    work in card componant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,65 @@
 
 import './App.css';
 import Card from './Card';
+import img1 from './images/card/cloud.jpeg'
+
 
 function App() {
 
   let arr = [
     {
       title: 'Cloud',
+      img:{img1},
       discription: 'some quick text here to show the product detail',
       price: '180 pkr',
     },
     {
       title: 'Bake',
+      img:{img1},
       discription: 'some quick text here to show the product detail',
       price: '120 pkr',
     },
     {
-      title: 'Ring',
+      title: 'cloud',
+      img:{img1},
       discription: 'some quick text here to show the product detail',
       price: '170 pkr',
     },
     {
-      title: 'Shall',
+      title: 'bake',
+      img:{img1},
       discription: 'some quick text here to show the product detail',
       price: '220 pkr',
     },
     {
-      title: 'Treat',
+      title: 'bun',
+      img:{img1},
       discription: 'some quick text here to show the product detail',
       price: '100 pkr',
-    }
+    },
+    {
+      title: 'pie',
+      img:{img1},
+      discription: 'some quick text here to show the product detail',
+      price: '100 pkr',
+    },
+    {
+      title: 'pizza',
+      img:{img1},
+      discription: 'some quick text here to show the product detail',
+      price: '100 pkr',
+    },
   ]
 
 
-console.log(arr[0].title)
-
-
+ 
   return (
     <div className="App">
-      <div>Hello world</div>
-      {arr.map((item) => {
-        for(let i=0;i<item;i++){
-          
-          <Card title={item[i].title}  price={item[i].price}  discription={item[i].discription}></Card>
-        }
+      {arr.map((item, index) => {
         
+        return <Card title={item.title} price={item.price} discription={item.discription} ind={index} img={item.img}></Card>
       }
-      
+
       )}
 
     </div>
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,9 +1,11 @@
 import './card.css'
 function Card(props){
+    console.log(props.title)
     return(
-        <div>
-            <div className='card-main-div'>
+        <div class='card'>
+            <div className='card-main-div' key={props.ind}>
                 <div className='card-Ttitle'>{props.title}</div>
+                <div className='card-img'> <img src={props.img} alt='images '></img></div>
                 <div className='card-discription'>{props.discription}</div>
                 <div className='card-price'>price : {props.price}</div>
                 <div className='card-btn'> Add to cart</div>
