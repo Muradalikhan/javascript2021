868a70847d54b9791db53a8aa832649626119670
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2021-10-25 13:17:05 +0500

    filter in react
    filter in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,83 +1,22 @@
 
 import './App.css';
-import Card from './Card';
-import img1 from './images/card/cloud.jpeg'
-import img2 from './images/card/bake.jpeg'
-import img3 from './images/card/bun.jpeg'
-import img4 from './images/card/cloud2.jpeg'
-import img5 from './images/card/pie.jpeg'
-// import img6 from './images/card/pocket.jpeg'
-import img7 from './images/card/pizza.jpeg'
-import img8 from './images/card/bake2.jpeg'
-
-
-
-
-let matchTitle=prompt('what do you want to search')
+import CardComp from './componant/card/CardComponant';
+import FilterComp from './componant/filter/filter';
 
 function App() {
 
 
-  let arr = [
-    {
-      title: 'Cloud',
-      img:img1,
-      discription: 'some quick text here to show the product detail',
-      price: '180 pkr',
-    },
-    {
-      title: 'bake',
-      img:img8,
-      discription: 'some quick text here to show the product detail',
-      price: '120 pkr',
-    },
-    {
-      title: 'cloud',
-      img:img4,
-      discription: 'some quick text here to show the product detail',
-      price: '170 pkr',
-    },
-    {
-      title: 'bake',
-      img:img2,
-      discription: 'some quick text here to show the product detail',
-      price: '220 pkr',
-    },
-    {
-      title: 'bun',
-      img:img3,
-      discription: 'some quick text here to show the product detail',
-      price: '100 pkr',
-    },
-    {
-      title: 'pie',
-      img:img5,
-      discription: 'some quick text here to show the product detail',
-      price: '100 pkr',
-    },
-    {
-      title: 'pizza',
-      img:img7,
-      discription: 'some quick text here to show the product detail',
-      price: '100 pkr',
-    },
-  ]
+  
 
 
  
   return (
     <div className="App">
-      {arr.map((item, index) => {
-        if(item.title===matchTitle){
-
-          return <Card title={item.title} price={item.price} discription={item.discription} ind={index} img={item.img}></Card>
-        }
-      }
-
-      )}
-
+     {/* <CardComp></CardComp> */}
+     <FilterComp></FilterComp>
+      
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ my-app/src/componant/card/CardComponant.js
@@ -0,0 +1,84 @@
+
+// import Card from './Card';
+// import img1 from './images/cloud.jpeg'
+// import img2 from './images/bake.jpeg'
+// import img3 from './images/bun.jpeg'
+// import img4 from './images/cloud2.jpeg'
+// import img5 from './images/pie.jpeg'
+// import img6 from './images/pocket.jpeg'
+// import img7 from './images/pizza.jpeg'
+// import img8 from './images/bake2.jpeg'
+
+
+
+
+// // let matchTitle=prompt('what do you want to search')
+
+// function CardComp() {
+
+
+//   let arr = [
+//     {
+//       title: 'Cloud',
+//       img:img1,
+//       discription: 'some quick text here to show the product detail',
+//       price: '180 pkr',
+//     },
+//     {
+//       title: 'bake',
+//       img:img8,
+//       discription: 'some quick text here to show the product detail',
+//       price: '120 pkr',
+//     },
+//     {
+//       title: 'cloud',
+//       img:img4,
+//       discription: 'some quick text here to show the product detail',
+//       price: '170 pkr',
+//     },
+//     {
+//       title: 'bake',
+//       img:img2,
+//       discription: 'some quick text here to show the product detail',
+//       price: '220 pkr',
+//     },
+//     {
+//       title: 'bun',
+//       img:img3,
+//       discription: 'some quick text here to show the product detail',
+//       price: '100 pkr',
+//     },
+//     {
+//       title: 'pie',
+//       img:img5,
+//       discription: 'some quick text here to show the product detail',
+//       price: '100 pkr',
+//     },
+//     {
+//       title: 'pizza',
+//       img:img7,
+//       discription: 'some quick text here to show the product detail',
+//       price: '100 pkr',
+//     },
+//   ]
+
+
+ 
+//   return (
+//     <div className="App">
+//       {arr.map((item, index) => {
+//         // if(item.title===matchTitle){
+
+//           return <Card title={item.title} price={item.price} discription={item.discription} ind={index} img={item.img}></Card>
+//         // }
+       
+
+//       }
+
+//       )}
+
+//     </div>
+//   );
+// }
+
+// export default CardComp;
