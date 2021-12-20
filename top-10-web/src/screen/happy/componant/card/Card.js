096280a84e06d392169a995de308fab7cadc84c3
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2021-12-20 23:53:56 +0500

    cards
    cards

--- top-10-web/src/screen/happy/componant/card/Card.js
+++ top-10-web/src/screen/happy/componant/card/Card.js
@@ -9,12 +9,12 @@ export default function Card() {
     let arr = ['card 1', 'card 2', 'card 3']
 
     return (
-        <div className='m-4 col-sm-12'>
+        <div className='my-4 col-sm-12'>
 
             {arr.map((card, index) => {
 
                 return (
-                    <div class="card mb-3 p-3 col-sm-4" style={{ maxWidth: "500px" }} key={index}>
+                    <div class="card m-3 p-3 col-sm-3 shadow-lg d-inline-block" style={{ maxWidth: "500px" }} key={index}>
                         <div class="row g-0">
                             <div class="col-md-6">
                                 <img src={img} class="img-fluid rounded-start" alt="img" style={{ width: '300', height: '300' }} />
