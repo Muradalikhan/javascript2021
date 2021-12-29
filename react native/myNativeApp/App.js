0e352426a3370ec7efb2feacaf74f47b98e50ebc
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2021-12-29 23:58:18 +0500

    todo design
    todo design

--- react native/myNativeApp/App.js
+++ react native/myNativeApp/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { styles } from './style'
 import Task from './componants/Task'
+import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView'
 
 
 
@@ -23,6 +24,19 @@ export default App = () => {
                     <Task text={'Task 2'}/>
                   </View>
               </View>
+
+              <KeyboardAvoidingView
+              behavior={Platform.OS==="ios"?'padding':'height'}
+              style={styles.writeTaskWrapper}
+              >
+                <TextInput placeholder='Write Task...' style={styles.input} />
+
+                <TouchableOpacity>
+                  <View style={styles.addWrapper}>
+                    <Text style={styles.addText}>+</Text>
+                  </View>
+                </TouchableOpacity>
+              </KeyboardAvoidingView>
         </View>
 
     </>
--- react native/myNativeApp/style.js
+++ react native/myNativeApp/style.js
@@ -119,39 +119,60 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   items: {
-    marginTop:20,
+    marginTop: 20,
   },
   item: {
-    backgroundColor:'#fff',
-    padding:15,
-    marginVertical:10,
-    borderRadius:10,
-    flexDirection:'row',
-    justifyContent:'space-between',
+    backgroundColor: '#fff',
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   leftItem: {
-    flexDirection:'row',
-    alignItems:'center',
+    flexDirection: 'row',
+    alignItems: 'center',
 
   },
   square: {
-    width:24,
-    height:24,
-    backgroundColor:'lightgrey',
-    marginRight:10,
+    width: 24,
+    height: 24,
+    backgroundColor: 'lightgrey',
+    marginRight: 10,
   },
   itemText: {
-    fontSize:16,
-    maxWidth:'80%',
+    fontSize: 16,
+    maxWidth: '80%',
 
   },
   circular: {
-    width:18,
-    height:18,
-    borderColor:'pink',
-    borderWidth:2,
-    borderRadius:8,
-    backgroundColor:'pink',
+    width: 18,
+    height: 18,
+    borderColor: 'pink',
+    borderWidth: 2,
+    borderRadius: 8,
+    backgroundColor: 'pink',
   },
+
+
+  writeTaskWrapper: {
+    position:'absolute',
+    bottom:60,
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+  input: {
+    padding:20,
+    backgroundColor:'#fff',
+    width:250,
+    borderRadius:60,
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+
+  },
+  addWrapper: {},
+  addText: {},
 
 })
