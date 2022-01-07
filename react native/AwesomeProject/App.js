c5b99fe96a7c8b173698bfafe32c1d03b8734fb2
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-01-07 17:55:46 +0500

    working
    working

--- react native/AwesomeProject/App.js
+++ react native/AwesomeProject/App.js
@@ -2,20 +2,21 @@
 
 import * as React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screen/homeScreen'
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Home" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <HomeScreen/>
   );
 }
 
