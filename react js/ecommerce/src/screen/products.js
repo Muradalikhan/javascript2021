5a78e62d69786309cf30a0723ac31c1f0d43cd49
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-02-09 00:49:21 +0500

    sidebar complete
    sidebar complete

--- react js/ecommerce/src/screen/products.js
+++ react js/ecommerce/src/screen/products.js
@@ -1,28 +1,22 @@
-
-
-import React from 'react';
-import Card from '../components/Card';
-import Cart from '../components/Cart';
-import Header from '../components/header';
-import { useSelector } from 'react-redux'
-import Header2 from '../components/header2';
-import SideBar from '../components/sideBar';
-
-
+import React from "react";
+import Card from "../components/Card";
+import Cart from "../components/Cart";
+import Header from "../components/header";
+import { useSelector } from "react-redux";
+import Header2 from "../components/header2";
+import SideBar from "../components/sideBar";
 
 function Products() {
-  const showCartstate=useSelector(state=>state.showCart.value)
+  const showCartstate = useSelector((state) => state.showCart.value);
 
   return (
-      <>
-      
-      <Header/>
-      <Header2/>
-      {/* {showCartstate?<Cart/>:null}
-      <Card/> */}
-      <SideBar/>
-      
-      </>
+    <>
+      <Header />
+      <Header2 />
+      {showCartstate ? <Cart /> : null}
+      <SideBar />
+      <Card />
+    </>
   );
 }
 
