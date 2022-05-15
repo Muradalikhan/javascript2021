7722f2bd611da5f1e2e97ac9b3fc8919cbe29770
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-05-15 21:55:13 +0500

    Create dashboardCard

---
+++ react js/top-10-web/src/components/DashboardCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function DashboardCard({ img, title, onClick }) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 445,
+        cursor: "pointer",
+        "&:hover": { boxShadow: "4px 4px 8px #444" },
+      }}
+      onClick={onClick}
+    >
+      <CardMedia
+        component="img"
+        height="280"
+        width="100%"
+        image={img}
+        alt="cardimg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
