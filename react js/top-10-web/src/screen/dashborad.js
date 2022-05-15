fd1567091663b34b9205418d70bdaa19309cf660
Murad khan <37217077+Muradalikhan@users.noreply.github.com>
2022-05-15 21:54:47 +0500

    updated dashboard

--- react js/top-10-web/src/screen/dashborad.js
+++ react js/top-10-web/src/screen/dashborad.js
@@ -1,25 +1,42 @@
-import { Link } from "react-router-dom";
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router";
+//icon
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import DashboardCard from "../components/DashboardCard";
+//img
+import img1 from "../asset/images/amazon.jpg";
+import img2 from "../asset/images/argos.jpg";
+import img3 from "../asset/images/happy.jpg";
 
 const drawerWidth = 240;
-
+const data = [
+  { img: img1, title: "Amazon", icon: AcUnitIcon },
+  { img: img2, title: "Argos", icon: CameraAltIcon },
+  { img: img3, title: "Happy", icon: FastfoodIcon },
+  { img: img1, title: "Amazon", icon: AcUnitIcon },
+  { img: img2, title: "Argos", icon: CameraAltIcon },
+  { img: img3, title: "Happy", icon: FastfoodIcon },
+  { img: img1, title: "Amazon", icon: AcUnitIcon },
+  { img: img2, title: "Argos", icon: CameraAltIcon },
+  { img: img3, title: "Happy", icon: FastfoodIcon },
+];
 function Dashboard(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -28,52 +45,35 @@ function Dashboard(props) {
     setMobileOpen(!mobileOpen);
   };
 
-  let naviagte=useNavigate()
-  const goToPage=(textMatch)=>{
-    if(textMatch==='Amazon'){
-        naviagte('./amazon')
-        console.log('i am run')
-    }
-    else if(textMatch==='Argos'){
-        naviagte('./argos')
-    }
-    else if(textMatch==='Happy'){
-        naviagte('./happy')
-    }
-  }
+  let naviagte = useNavigate();
+  const goToPage = (link) => {
+    const linkTo = link.toLowerCase();
+    naviagte(`/${linkTo}`);
+  };
 
   const drawer = (
     <div>
       <Toolbar />
       <Divider />
       <List>
-        {['Amazon', 'Argos','Happy', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} onClick={()=>(goToPage(text))}/>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
+        {data?.map((item, index) => (
+          <ListItem button key={index}>
+            <ListItemIcon>{<item.icon />}</ListItemIcon>
+            <ListItemText
+              primary={item.title}
+              onClick={() => goToPage(item.title)}
+            />
           </ListItem>
         ))}
       </List>
     </div>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -88,12 +88,12 @@ function Dashboard(props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+            Top 10 Projects
           </Typography>
         </Toolbar>
       </AppBar>
@@ -102,18 +102,20 @@ function Dashboard(props) {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -121,8 +123,11 @@ function Dashboard(props) {
         <Drawer
           variant="permanent"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
           open
         >
@@ -131,61 +136,31 @@ function Dashboard(props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Grid container spacing={5}>
+          {data?.map(({ img, title }, index) => (
+            <Grid item xs={12} md={6} lg={4} key={index}>
+              <DashboardCard
+                img={img}
+                title={title}
+                onClick={() => goToPage(title)}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Box>
   );
 }
 
 Dashboard.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
 export default Dashboard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
