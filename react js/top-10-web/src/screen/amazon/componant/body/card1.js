e95d1ad5fb33e4db9732f684a1c09577cec164f8
Murad khan <murad.jaddoon@gmail.com>
2022-05-22 03:29:01 +0500

    updated card1 of amozon

--- react js/top-10-web/src/screen/amazon/componant/body/card1.js
+++ react js/top-10-web/src/screen/amazon/componant/body/card1.js
@@ -1,15 +1,23 @@
-import img1  from '../../asset/img/product2.jpg'
-function Card1(){
-
-    return(
-        <>
-        <div style={{backgroundColor:'white',margin:'10px',padding:'20px',height:'450px',width:'350px',border:'1px solid lightgrey' }}>
-            <h2 style={{float:'left'}}>Heading</h2>
-            <img src={img1} alt='product'  height='330' width='300'></img>
-            <p  style={{float:'left',margin:'10px 0'}}><a href='#'>Discription Link</a></p>
-        </div>
-        </>
-    )
+function Card1({ title, img }) {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "white",
+          margin: "10px auto",
+          padding: "20px",
+          height: "450px",
+          minWidth: "300px",
+          maxWidth: "350px",
+          border: "1px solid lightgrey",
+        }}
+      >
+        <h2 style={{ float: "left" }}>{title}</h2>
+        <img src={img} alt="product" height="330" width="100%"></img>
+        <p style={{ float: "left", margin: "10px 0" }}>shop now</p>
+      </div>
+    </>
+  );
 }
 
-export default Card1
+export default Card1;
