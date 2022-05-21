186df89e29a72d4ae30ccd19a8d06263f65876e4
Murad khan <murad.jaddoon@gmail.com>
2022-05-22 03:28:41 +0500

    new route created for w3schools

--- react js/top-10-web/src/config/router/router.js
+++ react js/top-10-web/src/config/router/router.js
@@ -1,24 +1,24 @@
-import { BrowserRouter, Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AmazonMain from "../../screen/amazon/amazon";
 import ArgosMain from "../../screen/argos/argos";
 import Dashboard from "../../screen/dashborad";
 import MainPageOFGridWebsite from "../../screen/Grid website/main";
 import Happy from "../../screen/happy/happy";
+import W3schools from "../../screen/w3schools/w3schools";
 
-function RouterApp(){
-
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Dashboard/>}></Route>
-            <Route path='/amazon' element={<AmazonMain/>}></Route>
-            <Route path='/argos' element={<ArgosMain/>}></Route>
-            <Route path='/grid' element={<MainPageOFGridWebsite/>}></Route>
-            <Route path='/happy' element={<Happy/>}></Route>
-        </Routes>
-        
-        </BrowserRouter>
-    )
+function RouterApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/amazon" element={<AmazonMain />}></Route>
+        <Route path="/argos" element={<ArgosMain />}></Route>
+        <Route path="/grid" element={<MainPageOFGridWebsite />}></Route>
+        <Route path="/happy" element={<Happy />}></Route>
+        <Route path="/w3schools" element={<W3schools />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default RouterApp
+export default RouterApp;
