b4b48bb9a9d49e704d7207f06141eb15f7bec3c0
Murad khan <murad.jaddoon@gmail.com>
2022-06-19 15:19:24 +0500

    updated card js

--- react js/top-10-web/src/screen/argos/component/body/cards.js
+++ react js/top-10-web/src/screen/argos/component/body/cards.js
@@ -1,73 +1,69 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
 
 export default function Cards() {
-    return (
-
-        
-            <Grid container md={10} xs={12} sx={{ margin: '0 auto', alignItems: 'center', justifyItems: 'center' }}>
-                <Grid item md={4} xs={8} sx={{margin:'0 auto'}}>
-                    <Card sx={{ maxWidth: 445, margin: '4px' }}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                Murad random text
-                                </Typography>
-
-                            </CardContent>
-                        </CardActionArea>
-
-                    </Card>
-                </Grid>
-                <Grid item md={4} xs={8} sx={{margin:'0 auto'}}>
-                    <Card sx={{ maxWidth: 445, margin: '4px' }}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Murad random text
-                                </Typography>
-
-                            </CardContent>
-                        </CardActionArea>
-
-                    </Card>
-                </Grid>
-                <Grid item md={4} xs={8} sx={{margin:'0 auto'}}>
-                    <Card sx={{ maxWidth: 445, margin: '4px' }}>
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                Murad random text
-                                </Typography>
-
-                            </CardContent>
-                        </CardActionArea>
-
-                    </Card>
-                </Grid>
-            </Grid>
-        
-    );
+  return (
+    <Grid
+      container
+      md={10}
+      xs={12}
+      sx={{ margin: "0 auto", alignItems: "center", justifyItems: "center" }}
+    >
+      <Grid item md={4} xs={8} sx={{ margin: "0 auto" }}>
+        <Card sx={{ maxWidth: 445, margin: "4px" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Meal
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item md={4} xs={8} sx={{ margin: "0 auto" }}>
+        <Card sx={{ maxWidth: 445, margin: "4px" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Meal
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item md={4} xs={8} sx={{ margin: "0 auto" }}>
+        <Card sx={{ maxWidth: 445, margin: "4px" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Meal
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
